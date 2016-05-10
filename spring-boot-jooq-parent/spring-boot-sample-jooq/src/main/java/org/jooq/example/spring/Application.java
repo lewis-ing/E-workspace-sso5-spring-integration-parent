4155f11494462ee6e84e75aca2ce64db21b244f3
liuwenlong <liuwenlong@hanhua.com>
2016-05-10 14:29:15 +0800

    测试jooq案例

--- spring-boot-jooq-parent/spring-boot-sample-jooq/src/main/java/org/jooq/example/spring/Application.java
+++ spring-boot-jooq-parent/spring-boot-sample-jooq/src/main/java/org/jooq/example/spring/Application.java
@@ -1,13 +1,24 @@
 package org.jooq.example.spring;
 
+import org.jooq.example.spring.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	
+	@Autowired
+	private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		bookService.create(10, 1, "spring");
+	}
 
 }
--- spring-boot-jooq-parent/spring-boot-sample-jooq/src/main/java/org/jooq/example/spring/service/DefaultBookService.java
+++ spring-boot-jooq-parent/spring-boot-sample-jooq/src/main/java/org/jooq/example/spring/service/DefaultBookService.java
@@ -18,7 +18,9 @@ public class DefaultBookService implements BookService {
 
 		// This method has a "bug". It creates the same book twice. The second insert
 		// should lead to a constraint violation, which should roll back the whole transaction
-		for (int i = 0; i < 2; i++)
-			dsl.insertInto(BOOK).set(BOOK.ID, id).set(BOOK.AUTHOR_ID, authorId).set(BOOK.TITLE, title).execute();
+//		for (int i = 0; i < 2; i++)
+//			dsl.insertInto(BOOK).set(BOOK.ID, id).set(BOOK.AUTHOR_ID, authorId).set(BOOK.TITLE, title).execute();
+		
+		dsl.insertInto(BOOK).set(BOOK.ID, id).set(BOOK.AUTHOR_ID, authorId).set(BOOK.TITLE, title).execute();
 	}
 }
